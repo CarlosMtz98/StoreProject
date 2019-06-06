f3c2cc00c91dc3ac1d2049dd69c46d40077f82ec
Carlos <cmtz.rdz.98@gmail.com>
2019-06-06 15:43:47 -0500

    Product setup

---
+++ Store/Store/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+            
+        }
+    }
+}
---
+++ Store/Store/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Store.Models
+{
+    public class Product
+    {   
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string Category { get; set; }
+        public bool InStock { get; set; }
+    }
+}
